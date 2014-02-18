658fb37c6190dfa7f30e28febf721ed0c06eaa9a
Rueben Ramirez <ruebenramirez@gmail.com>
2014-02-17 23:51:11 -0500

    adding in some debug statements

--- index-wp-redis.php
+++ index-wp-redis.php
@@ -35,15 +35,15 @@ function getCleanUrl($secret) {
     $url = "http://${_SERVER['HTTP_HOST']}${_SERVER['REQUEST_URI']}";
     $current_url = str_replace($replaceKeys, '', $url);
     return $current_url;
-
 }
 
-$debug                  =  true;
-$cache                  =  false;
-$websiteIp              =  '127.0.0.1';
-$reddis_server          = '127.0.0.1';
-$secret_string          =  'changeme';
-
+$debug          = true;
+$cache          = true;
+$websiteIp      = '127.0.0.1';
+$reddis_server  = '127.0.0.1';
+$secret_string  = 'changeme';
+$current_url    = getCleanUrl($secret_string);
+$redis_key      = md5($current_url);
 
 handleCDNRemoteAddressing();
 
@@ -51,24 +51,31 @@ if(!defined('WP_USE_THEMES')) {
     define('WP_USE_THEMES', true);
 }
 
-$current_url = getCleanUrl($secret_string);
-$redis_key = md5($current_url);
-
 try {
     // check if PECL Extension is available
     if (class_exists('Redis')) {
+        if ($debug) {
+            echo "<!-- Redis PECL module found -->\n";
+        }
         $redis = new Redis();
 
         // Sockets can be used as well. Documentation @ https://github.com/nicolasff/phpredis/#connection
         $redis->connect($reddis_server);
         
     } else { // Fallback to predis5.2.php
+
+        if ($debug) {
+            echo "<!-- using predis as a backup -->\n";
+        }
         include_once("wp-content/plugins/wp-redis-cache/predis5.2.php"); //we need this to use Redis inside of PHP
         $redis = new Predis_Client();
     }
     
     //Either manual refresh cache by adding ?refresh=secret_string after the URL or somebody posting a comment
     if (refreshHasSecret($secret_string) || requestHasSecret($secret_string) || isRemotePageLoad($current_url, $websiteIp)) {
+        if ($debug) {
+            echo "<!-- manual refresh was required -->\n";
+        }
         $redis->del($redis_key);
         require('./wp-blog-header.php');
         
@@ -76,17 +83,23 @@ try {
         $seconds_cache_redis = get_option('wp-redis-cache-seconds',43200);
     // This page is cached, lets display it
     } else if ($redis->exists($redis_key)) {
+        if ($debug) {
+            echo "<!-- serving page from cache: key: $redis_key -->\n";
+        }
         $cache  = true;
         $html_of_page = $redis->get($redis_key);
         echo $html_of_page;
 
      // If the cache does not exist lets display the user the normal page without cache, and then fetch a new cache page
-    } else if ($_SERVER['REMOTE_ADDR'] != $ip_of_your_website && strstr($current_url, 'preview=true') == false) {
+    } else if ($_SERVER['REMOTE_ADDR'] != $websiteIp && strstr($current_url, 'preview=true') == false) {
+        if ($debug) {
+            echo "<!-- displaying page without cache -->\n";
+        }
         
         $isPOST = ($_SERVER['REQUEST_METHOD'] === 'POST') ? 1 : 0;
         
         $loggedIn = preg_match("/wordpress_logged_in/", var_export($_COOKIE, true));
-        if ($isPost == 0 && $loggedIn == 0) {
+        if (!$isPOST && !$loggedIn) {
             ob_start();
             require('./wp-blog-header.php');
             $html_of_page = ob_get_contents();
@@ -111,25 +124,29 @@ try {
             require('./wp-blog-header.php');
         }
         
-    } else if ($_SERVER['REMOTE_ADDR'] != $ip_of_your_website && strstr($current_url, 'preview=true') == true) {
+    } else if ($_SERVER['REMOTE_ADDR'] != $websiteIp && strstr($current_url, 'preview=true') == true) {
         require('./wp-blog-header.php');
     }
      // else {   // This is what your server should get if no cache exists  //deprecated, as the ob_start() is cleaner
         // require('./wp-blog-header.php');
     // }
 } catch (Exception $e) {
-    require('./wp-blog-header.php');
+    //require('./wp-blog-header.php');
+    echo "something went wrong";
 }
 
 $end  = microtime();
 $time = (@getMicroTime($end) - @getMicroTime($start));
 if ($debug) {
-    echo "<!-- Cache system by Benjamin Adams. Page generated in " . round($time, 5) . " seconds. -->";
-    echo "<!-- Site was cached  = " . $cache . " -->";
-    echo "<!-- wp-redis-cache-seconds  = " . $seconds_cache_redis . " -->";
-    echo "<!-- wp-redis-cache-secret  = " . $secret_string . "-->";
-    echo "<!-- wp-redis-cache-ip  = " . $ip_of_your_website . "-->";
-    echo "<!-- wp-redis-cache-unlimited = " . $unlimited . "-->";
-    echo "<!-- wp-redis-cache-debug  = " . $debug . "-->";
+    echo "<!-- Cache system by Benjamin Adams. Page generated in " . round($time, 5) . " seconds. -->\n";
+    echo "<!-- Site was cached  = " . $cache . " -->\n";
+    if (isset($seconds_cache_redis)) {
+        echo "<!-- wp-redis-cache-seconds  = " . $seconds_cache_redis . " -->\n";
+    }
+    echo "<!-- wp-redis-cache-secret  = " . $secret_string . "-->\n";
+    echo "<!-- wp-redis-cache-ip  = " . $websiteIp . "-->\n";
+    if (isset($unlimited)) {
+        echo "<!-- wp-redis-cache-unlimited = " . $unlimited . "-->\n";
+    }
+    echo "<!-- wp-redis-cache-debug  = " . $debug . "-->\n";
 }
-
